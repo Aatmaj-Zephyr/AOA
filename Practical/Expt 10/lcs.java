264440088218e275f3e98f40d7c1d7d50f7d4a14
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-04-24 11:49:08 +0530

    Update lcs.java

--- Practical/Expt 10/lcs.java
+++ Practical/Expt 10/lcs.java
@@ -1,7 +1,7 @@
-public class lcs {
+public class Main {
     public static void main(String args[]){
-        char [] str1= {'a','b','c','d','d','d'};
-        char [] str2 = {'a','d','c'};
+        char [] str1= {'a','b','c','d','e','d'};
+        char [] str2 = {'a','b','c','e','d'};
         int arr[][]=new int[str1.length+1][str2.length+1];
         for(int i=0;i<str1.length;i++){
             for(int j=0;j<str2.length;j++){
@@ -17,12 +17,54 @@ public class lcs {
                 }
             }
         }
-       
+        int p=0;
+        int q=0;
+        
+        while(0==0){
+            if(p>=str1.length-1&q>=str2.length-1){
+                if(str1[p]==str2[q]){
+                  System.out.println(str1[p]);
+                }
+                break;
+            }
+           if(p>=str1.length-1){
+               p--;
+               q++;
+               
+               }
+               if(q>=str2.length-1){
+               q--;
+               p++;
+            
+                 }
+             //   System.out.println(str1[p]+","+str2[q]+","+arr[p+1][q+1]+","+p+","+q);
+                if(str1[p]==str2[q]){
+                  System.out.println(str1[p]);
+                  p++;
+                  q++;
+                //  System.out.print(","+p+","+q+"\n");
+                }
+                else{
+                    if(arr[p][q+1]<=arr[p+1][q])
+                    {
+                     //  System.out.println(arr[p+1][q]);
+                        p++;
+                    }
+                
+                else if(arr[p][q+1]>arr[p+1][q])
+                    {
+                      //  System.out.println(arr[p][q+1]);
+                        q++;
+                    }
+                }
+                
+            
+        }
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]);
+               // System.out.print(arr[i][j]);
             }
-            System.out.println();
+         //   System.out.println();
         }
     }
     
